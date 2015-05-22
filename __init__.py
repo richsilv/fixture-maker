8e3e519b36351b2a794e14dafbdaf3dfb17436e0
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2015-05-22 18:15:55 +0100

    supressed saving of non-JSON object in session that was causing exception

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, make_response
+from flask import Flask, render_template, request, session, make_response, redirect
 import fixtures
 app = Flask(__name__)
 app.config.update(DEBUG = True)
@@ -15,10 +15,11 @@ def get_fixtures():
     if rdata['optpitches'] == "true":
         fixt = fixtures.optimise_pitches(fixt, 2000)
     if rdata['optrounds'] == "true":
-        fixt = fixtures.optimise_rounds(fixt, 2000)    
-    session['fixt'] = fixt
+        fixt = fixtures.optimise_rounds(fixt, 2000)
+    # session['fixt'] = fixt
     fixtures.save_fixtures('templates/fixtures.csv', fixt)
     return render_template('render_fixt.html', pitches=range(1, int(rdata['pitches']) + 1), fixtures=[[(r[0][i], r[1][i]) for i in range(len(r[0]))] for r in fixt])
+    return "hello"
 
 @app.route("/download/")
 def download():
